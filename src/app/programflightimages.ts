67ff347d2b95752add23d2c2131476d67dafcf04
nidheesha <35656880+nidheesha@users.noreply.github.com>
2018-05-09 14:47:25 +0530

    .
    .

--- src/app/programflightimages.ts
+++ src/app/programflightimages.ts
@@ -1,90 +1,11 @@
-// export const pfimages:any[]=[
-// {'serial':1,'path':'../../assets/Program flight.png','content':'Programmed Flight path '},
-// {'serial':2,'path':'../../assets/linearprogramflight.png','content':'Linear Program Flight'},
-// {'serial':3,'path':'../../assets/circular_programflight.png','content':'Circular Program Flight'}
-// ];
 
 export const pfimages : any[]=[
 {
 'JobID':"Job-1",
 'AssetID':"Asset-1",
-'images':[
-			{'serial':1,'path':'../../assets/Program flight.png','content':'Programmed Flight path '},
-			{'serial':2,'path':'../../assets/linearprogramflight.png','content':'Linear Program Flight'},
-			//{'serial':3,'path':'../../assets/circular_programflight.png','content':'Circular Program Flight'}
-			]
-},
-{
-'JobID':"Job-1",
-'AssetID':"Asset-2",
-'images':[
-			{'serial':1,'path':'../../assets/Program flight.png','content':'Programmed Flight path '},
-			{'serial':2,'path':'../../assets/linearprogramflight.png','content':'Linear Program Flight'},
-			{'serial':3,'path':'../../assets/circular_programflight.png','content':'Circular Program Flight'}
-			]
-},
-{
-'JobID':"Job-1",
-'AssetID':"Asset-3",
-'images':[
-			{'serial':1,'path':'../../assets/Program flight.png','content':'Programmed Flight path '},
-			{'serial':2,'path':'../../assets/linearprogramflight.png','content':'Linear Program Flight'},
-			{'serial':3,'path':'../../assets/circular_programflight.png','content':'Circular Program Flight'}
-			]
-},
-{
-'JobID':"Job-2",
-'AssetID':"Asset-1",
-'images':[
-			{'serial':1,'path':'../../assets/Program flight.png','content':'Programmed Flight path '},
-			{'serial':2,'path':'../../assets/linearprogramflight.png','content':'Linear Program Flight'},
-			{'serial':3,'path':'../../assets/circular_programflight.png','content':'Circular Program Flight'}
+'images':[//give image data as
+			//{'serial':1,'path':'','content':''},
 			
-			]
-},
-{
-'JobID':"Job-2",
-'AssetID':"Asset-2",
-'images':[
-			{'serial':1,'path':'../../assets/Program flight.png','content':'Programmed Flight path '},
-			{'serial':2,'path':'../../assets/linearprogramflight.png','content':'Linear Program Flight'},
-			{'serial':3,'path':'../../assets/circular_programflight.png','content':'Circular Program Flight'}
-			]
-},
-{
-'JobID':"Job-2",
-'AssetID':"Asset-3",
-'images':[
-			{'serial':1,'path':'../../assets/Program flight.png','content':'Programmed Flight path '},
-			{'serial':2,'path':'../../assets/linearprogramflight.png','content':'Linear Program Flight'},
-			{'serial':3,'path':'../../assets/circular_programflight.png','content':'Circular Program Flight'}
-			]
-},
-{
-'JobID':"Job-3",
-'AssetID':"Asset-1",
-'images':[
-			{'serial':1,'path':'../../assets/Program flight.png','content':'Programmed Flight path '},
-			{'serial':2,'path':'../../assets/linearprogramflight.png','content':'Linear Program Flight'},
-			{'serial':3,'path':'../../assets/circular_programflight.png','content':'Circular Program Flight'}
-			]
-},
-{
-'JobID':"Job-3",
-'AssetID':"Asset-2",
-'images':[
-			{'serial':1,'path':'../../assets/Program flight.png','content':'Programmed Flight path '},
-			{'serial':2,'path':'../../assets/linearprogramflight.png','content':'Linear Program Flight'},
-			{'serial':3,'path':'../../assets/circular_programflight.png','content':'Circular Program Flight'}
-			]
-},
-{
-'JobID':"Job-3",
-'AssetID':"Asset-3",
-'images':[
-			{'serial':1,'path':'../../assets/Program flight.png','content':'Programmed Flight path '},
-			{'serial':2,'path':'../../assets/linearprogramflight.png','content':'Linear Program Flight'},
-			{'serial':3,'path':'../../assets/circular_programflight.png','content':'Circular Program Flight'}
 			]
 }
 
